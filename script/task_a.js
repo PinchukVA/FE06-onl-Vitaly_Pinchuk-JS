ca5dd80c666df307fc078d4167a97220ca1abe76
pinchuk.v.a <vitaly.pin4uck@yandex.ru>
2021-02-11 15:32:18 +0300

    add task b

--- script/task_a.js
+++
@@ -1,6 +0,0 @@
-var str = "I`am string";
-alert(str);
-console.log(str);
-console.log(str, typeof str);
-var str = "new string 'task_a'"
-console.log(str);
---
+++ script/task_b.js
@@ -0,0 +1,18 @@
+// task b 
+var nmb = 10; 
+var str = "I`am string";
+var bol = true;
+var und;
+var Bint = 123490n;
+var nl = null;
+var user = { name: "Виталий" };
+alert(nmb);
+alert(str);
+alert(bol);
+alert(und);
+alert(Bint);
+alert(nl);
+
+console.log(user);
+console.log(typeof user);
+
